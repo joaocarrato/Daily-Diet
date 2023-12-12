6734152db581953e07c0a03ddc6cb7e179c3aa5d
joaocarrato <joaocarrato18@gmail.com>
2023-12-12 13:47:42 -0300

    feat: initial commit

---
+++ src/themes/themes.ts
@@ -0,0 +1,25 @@
+export const colors = {
+  product: {
+    redDark: '#BF3B44',
+    redMid: '#F3BABD',
+    redLight: '#F4E6E7',
+    greenDark: '#639339',
+    greenMid: '#CBE4B4',
+    greenLight: '#E5F0DB',
+  },
+  base: {
+    gray100: '#1B1D1E',
+    gray200: '#333638',
+    gray300: '#5C6265',
+    gray400: '#B9BBBC',
+    gray500: '#DDDEDF',
+    gray600: '#EFF0F0',
+    gray700: '#FAFAFA',
+    white: '#FFFFFF',
+  },
+};
+
+export const fonts = {
+  bold: 'NunitoSans_7pt-Bold',
+  regular: 'NunitoSans_7pt-Regular',
+};
