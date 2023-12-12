07c9184d6aa2434a21e277461ed1d24b109de1d6
joaocarrato <joaocarrato18@gmail.com>
2023-12-12 16:32:48 -0300

    :fire: Removing icon files since lib is being used

--- src/themes/icon.ts
+++
@@ -1,5 +0,0 @@
-import IllustrationBad from '../assets/images/illustration_bad.svg';
-import IllustrationSuccess from '../assets/images/illustration_success.svg';
-import Logo from '../assets/images/logo.svg';
-
-export { IllustrationBad, IllustrationSuccess, Logo };
