c99e8b6f5617626ed9c02947d0f7d006ebf73eac
joaocarrato <joaocarrato18@gmail.com>
2023-12-14 16:14:00 -0300

    :test_tube: test: initial configs

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'node', 'json'],
 };
+
+module.exports = config;
